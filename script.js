c34e1e37cb4bc8ca88634848cf60873d1c9f036e
dominik-kiss <dominik.kiss997@gmail.com>
2022-09-15 16:51:56 +0200

    Fix re-sizing bug, add function for color-darkening/shading
    Fix bug where grid size resets to 16 when switching modes.
    Add function for adjusting color with a certain value.

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ function newGrid() {
     }
   }
   else {
+    let currentSize = Math.sqrt(container.childElementCount);
     removeGrid();
-    makeGrid();
+    makeGrid(currentSize);
   }
   
   gridItems = document.querySelectorAll(".grid-item");
@@ -73,6 +74,7 @@ function randomNum() {
 }
 
 function changeColor() {
+  let startingColor = "hsl(0, 100%, 100%)";
   switch (mode) {
     case 1:
       this.style.backgroundColor = "black";
@@ -81,9 +83,39 @@ function changeColor() {
       this.style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
       break;
     case 3:
-      //Darken background-color by 10%
+      this.style.backgroundColor = "";
       break;
   }
+}
+
+function LightenDarkenColor(col, amt) {
+  
+  var usePound = false;
+
+  if (col[0] == "#") {
+      col = col.slice(1);
+      usePound = true;
+  }
+
+  var num = parseInt(col,16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00FF) + amt;
+
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+
+  var g = (num & 0x0000FF) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
 }
 
 makeGrid();
